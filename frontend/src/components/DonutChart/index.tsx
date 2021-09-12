f18ae49d93cf9f1834cfa90de18711421cf900da
RafaelRibeiroEstrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2021-09-12 09:05:55 -0400

    Update

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
